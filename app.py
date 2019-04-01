b7bff428210713303e0b3fb0a901f7d938eca96b
BuildTools <unconfigured@null.spigotmc.org>
2019-03-31 20:02:07 -0400

    This won't work.

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def webhook():
 
   # We don't want to reply to ourselves!
   if data['name'] != 'Test Bot':
-    if any(x in textToParse for x in ['group chat', ' gc ']):
+    if any([x in textToParse for x in ['group chat', ' gc ']]):
       msg = 'Speaking of group chats, {}, check out the group chat list!'.format(data['name'])
       send_message(msg)
     if
