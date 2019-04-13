31e99ef5701f4ecfd33c298e2433d38c016811ea
rodrigo-castellon <rodrigo.castellon@pinecrest.edu>
2019-04-13 10:51:48 -0400

    edits

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import os
 import json
 
+import retrieve_msgs
+
+from datetime import datetime
+
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 
@@ -9,25 +13,34 @@ from flask import Flask, request
 app = Flask(__name__)
 
 commands = {
-    'help': 'commands: !help, !social, !gc',
+    'help': 'Here are some of my commands: !help, !social, !gc',
     'gc': 'Find other admitted Stanford group chats on this list: https://bit.ly/2FuzbPs',
     'social': 'Find other admitted trees\' social medias on this spreadsheet: '
 }
 
 BOT_NAME = 'Botty McBotFace'
 
+GROUP_NAME = 'Bot Testing'
+
+last_updated = datetime(2019, 1, 1)
+
 @app.route('/', methods=['POST'])
 def webhook():
     data = request.get_json()
     text = data['text'].lower()
 
+    if (datetime.now() - last_updated).seconds > 3600:
+        last_updated = datetime.now()
+        #retrieve_msgs.main(False, False, )
 
-    if data['name'] != BOT_NAME and text[0] == '!':
-        command = text[1:]
-        if command in commands.keys():
-            send_message(commands[command])
-        else:
-            send_message('Sorry, I don\'t know what that command is. Try again?')
+
+    if data['name'] != BOT_NAME:
+        if text[0] == '!':
+            command = text[1:]
+            if command in commands.keys():
+                send_message(commands[command])
+            else:
+                send_message('Sorry, I don\'t know what that command is. Try again?')
 
     return "ok", 200
 
