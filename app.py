701ebc703d784ab71212291f6ffabefdbc0cf79e
rodrigo-castellon <rodrigo.castellon@pinecrest.edu>
2019-04-12 21:47:12 -0400

    edits

--- app.py
+++ app.py
@@ -8,16 +8,24 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+commands = {
+    'help': 'commands: !help, !social, !gc',
+    'gc': 'Find other admitted Stanford group chats on this list: https://bit.ly/2FuzbPs',
+    'social': 'Find other admitted trees\' social medias on this spreadsheet: '
+}
+
 @app.route('/', methods=['POST'])
 def webhook():
     data = request.get_json()
     text = data['text'].lower()
 
     # We don't want to reply to ourselves!
-    if data['name'] != 'Botty McBotFace':
-        if any(x in text for x in ['group chat', 'gc']):
-            msg = 'Speaking of group chats, check out the group chat list: https://bit.ly/2FuzbPs'
-            send_message(msg)
+    if data['name'] != 'Botty McBotFace' and text[0] == '!':
+        command = text[1:]
+        if command in commands.keys():
+            send_message(commands[command])
+        else:
+            send_message('Sorry, I don\'t know what that command is. Try again?')
 
     return "ok", 200
 
