ed0214515f694b79c9d77109a0986102144b8d02
rodrigo-castellon <rodrigo.castellon@pinecrest.edu>
2019-04-14 23:55:27 -0400

    edits

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 from flask import Flask, request, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
 
 last_updated = datetime(2019, 1, 1)
 
