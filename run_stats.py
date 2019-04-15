50f3cd1f8d97531dfba83a7c6dfb1eeca927b1de
rodrigo-castellon <rodrigo.castellon@pinecrest.edu>
2019-04-14 23:22:06 -0400

    edits

--- run_stats.py
+++ run_stats.py
@@ -9,19 +9,19 @@ by retrieve_msgs.py as input.
 import os
 import psycopg2
 
-"""
-Given a phrase, return a function that is passed to readCsv to count the number
-of occurrences of a certain phrase.
-
-Params:
-phrase - a single string or a list of strings to match for
-count_dups - count multiple instances of a phrase in a message as a single instance
-print_matches - print the messages that match the phrase
-match_exactly - the message must match the phrase exactly
-print_user - when print_matches is true, only print the matches by this user
-
-"""
 def get_occurrences(phrase, count_dups=False, print_matches=False, match_exactly=False, print_user=None):
+    """
+    Given a phrase, return a function that is passed to read_db to count the number
+    of occurrences of a certain phrase.
+
+    Parameters:
+    phrase: a single string or a list of strings to match for
+    count_dups (boolean): count multiple instances of a phrase in a message as a single instance
+    print_matches (boolean): print the messages that match the phrase
+    match_exactly (boolean): the message must match the phrase exactly
+    print_user (boolean): when print_matches is true, only print the matches by this user
+
+    """
     def get_num(user, original_text):
         count = 0
         if original_text is None:
@@ -55,8 +55,10 @@ def num_words(user, text):
 def num_chars(user, text):
     return len(text)
 
-""" Reads the table `msgcounts` from the database and passes the content to process_msg_func """
 def read_db(process_msg_func=None):
+    """
+    Read the table `msgcounts` from the database and passes the content to process_msg_func.
+    """
     if process_msg_func == None:
         process_msg_func = lambda x,y: 1
 
@@ -78,52 +80,54 @@ def read_db(process_msg_func=None):
         d[user].append(data)
     return d
 
-""" Helper function that calls read_db and get_stats """
 def show_stats(func=None, **kwargs):
+    """Call `read_db` and `get_stats`"""
     result = read_db(func)
     return get_stats(result, **kwargs)
 
-""" 
-Given the return value of read_db, display useful stats 
-
-Params:
-data - dictionary where the key is the user's name and the value is a list that contains an
-        integer for each message by the user (typically representing the # of phrases matched)
-include_groupme - include messages sent by GroupMe
-total - sum the list of integers rather than take the average. If we're counting phrase 
-        occurrences, then total=True. If we are counting avg message length, then total=False
-percent - display the number of matches as a percentage of total messages
-compact - don't return the total num of messages and percentage
-"""
 def get_stats(data, include_groupme=False, total=True, percent=True, compact=True):
+    """ 
+    Given the return value of read_db, display useful stats.
+
+    Parameters:
+    data (dict): key is the user's name and the value is a list that contains an
+                 integer for each message by the user (typically representing the # of phrases matched)
+    include_groupme (boolean): include messages sent by GroupMe
+    total (boolean): If this isn't true, then we take the average of integer data. If we're counting phrase 
+                     occurrences, then total=True. If we are counting avg message length, then total=False
+    percent (boolean): display the number of matches as a percentage of total messages
+    compact (boolean): don't return the total num of messages and percentage
+
+    Returns:
+    A list `l` where each element contains statistics for each person.
+    """
     l = []
-    num_people = total_msgs = total_data_per_person = total_data = 0
-    for k,v in data.items():
-        if not include_groupme and str(k) == 'GroupMe':
+    total_stats = [-1, 0, 0]
+    num_people = len(data.keys())
+    total_msgs = total_data_per_person = total_data = 0
+    for user, ints in data.items():
+        if not include_groupme and user == 'GroupMe':
             continue
-        num_people += 1
-        num_msgs = len(v)
-        total_msgs += num_msgs
-        total_data_per_person = sum(v)
-        total_data += total_data_per_person
-        if total:
-            if percent:
-                l.append((str(k), num_msgs, total_data_per_person, 
-                    str(round(total_data_per_person * 100.0 / num_msgs, 1))+'%'))
-            else:
-                l.append((str(k), num_msgs, total_data_per_person))
-        else:
-            avg_data_len = 0
-            if num_msgs != 0:
-                avg_data_len = round(sum(v)*1.0 / num_msgs, 1)
-            l.append((str(k), num_msgs, avg_data_len))
-    l = sorted(l, key=lambda k:k[2], reverse=True)
-#   if total:
-#       l.append(('Average', round(total_msgs * 1.0 / num_people, 1), round(total_data * 1.0 / num_people, 1)))
-#       l.append(('Total', total_msgs, total_data))
-#   else:
-#       l.append(('Average', round(total_msgs * 1.0 / num_people, 1), round(total_data * 1.0 / total_msgs, 1)))
-#       l.append(('Total', total_msgs))
-    if compact:
-        return [(x[0],x[2]) for x in l]
-    return l
+        # stats: username, # of messages, total data, data per msg
+        user_stats = (user, len(ints), sum(ints), sum(ints)/len(ints))
+        total_stats[1:] = [user_stats[i] + x for i,x in enumerate(total_stats[1:])]
+        l.append(user_stats)
+
+    l = sorted(l, key=lambda k:k[1], reverse=True)
+
+    return l, total_stats
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
